9817c69af325d63e30abb56399508cd78beb7805
FendyPierre <fendy_tester@hotmail.com>
2018-11-02 20:37:28 -0500

    added blog and / route

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
+//App config
 var  express    = require("express"),
      mongoose   = require("mongoose")
      bodyParser  = require("body-parser")
      app        = express();
 
+     
 mongoose.connect("mongodb://localhost/blogapp");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+//Mongoose Model Config
+var blogSchema = mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+
+//RESTFUL Routes
+
+app.get("/blogs", function(req,res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("Failed to find blogs. ERROR:" + err);
+        }
+        else{
+            res.render("index", {blogs: blogs});
+        }
+    });
+
+});
+
+app.get("/", function(req,res){
+    res.redirect("/blogs");
+});
+
+
+
+
 
 app.listen(3000, function(){
     console.log("Server is running!");
